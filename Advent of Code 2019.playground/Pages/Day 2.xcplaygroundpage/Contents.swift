a2e921b59201a87f06ea0f2e07b658ae88acab67
James Coleman <james.coleman@everylifetechnologies.com>
2019-12-03 08:39:20 +0000

    Completed Day 2 Part 2

--- Advent of Code 2019.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ Advent of Code 2019.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -90,10 +90,10 @@ do {
 //    let processed3 = try processedArray(starting: array3)
 //    let processed4 = try processedArray(starting: array4)
     
-//    print(processed1)
-//    print(processed2)
-//    print(processed3)
-//    print(processed4) // [30, 1, 1, 4, 2, 5, 6, 0, 99] (should be [30,1,1,4,2,5,6,0,99]
+//    print(processed1) // [2, 0, 0, 0, 99]
+//    print(processed2) // [2, 3, 0, 6, 99]
+//    print(processed3) // [2, 4, 4, 5, 99, 9801]
+//    print(processed4) // [30, 1, 1, 4, 2, 5, 6, 0, 99]
 } catch {
     print(error)
 }
@@ -101,6 +101,8 @@ do {
 let puzzleInput = """
 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,2,23,10,27,1,27,5,31,1,31,6,35,1,6,35,39,2,39,13,43,1,9,43,47,2,9,47,51,1,51,6,55,2,55,10,59,1,59,5,63,2,10,63,67,2,9,67,71,1,71,5,75,2,10,75,79,1,79,6,83,2,10,83,87,1,5,87,91,2,9,91,95,1,95,5,99,1,99,2,103,1,103,13,0,99,2,14,0,0
 """
+
+// MARK: Part 1
 
 do {
     var puzzleInputAsArray = try numberLineAsArray(input: puzzleInput)
@@ -116,3 +118,44 @@ do {
 } catch {
     print(error)
 }
+
+// MARK: Part 2
+
+let puzzleInputAsArray = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 6, 19, 1, 9, 19, 23, 2, 23, 10, 27, 1, 27, 5, 31, 1, 31, 6, 35, 1, 6, 35, 39, 2, 39, 13, 43, 1, 9, 43, 47, 2, 9, 47, 51, 1, 51, 6, 55, 2, 55, 10, 59, 1, 59, 5, 63, 2, 10, 63, 67, 2, 9, 67, 71, 1, 71, 5, 75, 2, 10, 75, 79, 1, 79, 6, 83, 2, 10, 83, 87, 1, 5, 87, 91, 2, 9, 91, 95, 1, 95, 5, 99, 1, 99, 2, 103, 1, 103, 13, 0, 99, 2, 14, 0, 0]
+
+func programOutput(program: [Int], noun: Int, verb: Int) throws -> Int {
+    var program = program
+    
+    program[1] = noun
+    program[2] = verb
+    
+    let processed = try processedArray(starting: program)
+    
+    return processed[0]
+}
+
+do {
+//    let part1Answer = try programOutput(program: puzzleInputAsArray, noun: 12, verb: 2)
+//    print(part1Answer) // 3931283 (confirms that func is working properly
+    
+    /*
+    outer:
+    for noun in 0...99 {
+        for verb in 0...99 {
+            let answer = try programOutput(program: puzzleInputAsArray, noun: noun, verb: verb)
+            
+            if answer == 19690720 {
+                print(noun, verb, (100 * noun) + verb) // 69 79 6979 (correct!) (6980 times)
+                break outer
+            }
+        }
+    }
+    */
+    
+    let part2 = try programOutput(program: puzzleInputAsArray, noun: 69, verb: 79)
+    
+} catch {
+    print(error)
+}
+
+
